9451650cf06ab2bdc46c9817c65f9ef523df92fd
Ankit_Kumar_Singh <63195545+Barman7085@users.noreply.github.com>
2021-11-04 00:34:44 +0530

    Add files via upload
    create a folder named as routes and place 404.hml register.html colors.html and naruto.html into routes folder

---
+++ returnAnime.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const http = require('http');
+
+const server = http.createServer((req,res) => {
+	console.log("connected!");
+	console.log(req.url,req.method);
+	res.setHeader('Content-Type', 'text/html');
+	let path = './return2/';
+
+		switch(req.url)     
+		{
+			case '/' :
+				path += 'register.html';
+				break;
+			case '/naruto' :
+				path += 'naruto.html';
+				break;	
+			case '/colors' :
+				path += 'colors.html';
+				break;
+			case '/colors-update' :
+				res.statusCode = 301;
+				res.setHeader('Location', '/colors');
+				res.end();
+				break;
+			default :
+				path += '404.html';
+				break;			
+		}
+
+	fs.readFile(path, (err,data) => {
+		if(err) 
+		{
+			console.log(err);
+		}
+		else 
+		{
+			res.write(data);
+			res.write("<p>thats all!</p>");
+			res.end();
+		}
+	})
+});
+
+server.listen('3456', 'localhost',() => {
+	console.log("server initiated!!")
+});
